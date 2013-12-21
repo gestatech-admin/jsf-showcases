a594c9e162bec6fb6adb4aecca97e2d613e482c3
pkonyves <paul.konyves+github@gmail.com>
2013-12-21 17:48:01 +0100

    list xhtml pages in index.xhtml

---
+++ jsf-showcases.web/src/main/java/hu/palkonyves/jsfshowcase/index/Index.java
@@ -0,0 +1,40 @@
+package hu.palkonyves.jsfshowcase.index;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class Index {
+
+    public List<String> getPages() {
+	FacesContext facesCtx = FacesContext.getCurrentInstance();
+	ExternalContext externalContext = facesCtx.getExternalContext();
+
+	List<String> result = new ArrayList<String>();
+	extractXhtmls(externalContext, "/", result);
+	return result;
+    }
+
+    private void extractXhtmls(ExternalContext externalContext,
+	    String pathPrefix, List<String> result) {
+
+	Set<String> resourcePaths = externalContext
+		.getResourcePaths(pathPrefix);
+	for (String path : resourcePaths) {
+	    if (path.endsWith(".xhtml")) {
+		// if xhtml, append
+		result.add(path);
+	    } else if (path.endsWith("/")) {
+		// if directory, recurse
+		extractXhtmls(externalContext, path, result);
+	    }
+	}
+    }
+}
