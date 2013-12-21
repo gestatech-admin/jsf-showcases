89b03678dd7b5c45b0eab1246835ad82b07912e1
pkonyves <paul.konyves+github@gmail.com>
2013-12-21 20:16:15 +0100

    added google prettify and show source code on page

--- jsf-showcases.web/src/main/java/hu/palkonyves/jsfshowcase/index/Index.java
+++ jsf-showcases.web/src/main/java/hu/palkonyves/jsfshowcase/index/Index.java
@@ -1,28 +1,67 @@
 package hu.palkonyves.jsfshowcase.index;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
+import org.apache.commons.io.IOUtils;
+
 @Named
 @RequestScoped
 public class Index {
+
+    private List<String> pages;
+
+    @PostConstruct
+    protected void fetchPages() {
+	ExternalContext externalContext = getExternalContext();
+
+	List<String> result = new ArrayList<String>();
+	extractXhtmls(externalContext, "/", result);
+	pages = result;
+    }
+
+    private ExternalContext getExternalContext() {
+	FacesContext facesCtx = FacesContext.getCurrentInstance();
+	ExternalContext externalContext = facesCtx.getExternalContext();
+	return externalContext;
+    }
 
     /**
      * @return list of xhtml pages within the war
      */
     public List<String> getPages() {
-	FacesContext facesCtx = FacesContext.getCurrentInstance();
-	ExternalContext externalContext = facesCtx.getExternalContext();
+	return pages;
+    }
 
-	List<String> result = new ArrayList<String>();
-	extractXhtmls(externalContext, "/", result);
-	return result;
+    public String getSource(String pageName) throws IOException {
+
+	for (String pageUrl : pages) {
+	    if (pageUrl.endsWith(pageName)) {
+		ExternalContext externalContext = getExternalContext();
+		InputStream resourceAsStream = externalContext.getResourceAsStream(pageUrl);
+		return inputStreamToString(resourceAsStream);
+	    }
+	}
+
+	return null;
+    }
+
+    private String inputStreamToString(InputStream inputStream)
+	    throws IOException {
+	StringWriter writer = new StringWriter();
+	IOUtils.copy(inputStream, writer, "UTF8");
+	return writer.toString();
+
     }
 
     /**
