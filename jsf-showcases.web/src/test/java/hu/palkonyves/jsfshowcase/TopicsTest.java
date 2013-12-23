51debaea6b9f94d8f4e8f7b0a83a456a924ce98c
pkonyves <paul.konyves+github@gmail.com>
2013-12-23 01:33:45 +0100

    fine visual of source codes on bottom

---
+++ jsf-showcases.web/src/test/java/hu/palkonyves/jsfshowcase/TopicsTest.java
@@ -0,0 +1,38 @@
+package hu.palkonyves.jsfshowcase;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class TopicsTest {
+
+    private static final Pattern H1_PATTERN = Pattern
+            .compile("\\<h1\\>(.*)\\</h1\\>");
+
+    private static final String TOPIC = "toptop";
+
+    private final String source = "<html>\n" + "<body>\n"
+            + "<div> something </div>\n" + "<h1>" + TOPIC + "</h1>\n"
+            + "</body>\n" + "</html>";
+
+
+    @Test
+    public void getTopicsTest() {
+
+        String[] lines = source.split("\\r?\\n");
+        String matching = null;
+        for (String line : lines) {
+            Matcher h1Matcher = H1_PATTERN.matcher(line);
+            if (h1Matcher.matches()) {
+                matching = h1Matcher.group(1);
+            }
+        }
+
+        System.out.println(matching);
+        Assert.assertEquals(TOPIC, matching);
+    }
+
+}
