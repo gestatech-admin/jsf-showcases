996e4d0025fd2829d6a078c771338cb9f18d619b
pkonyves <paul.konyves+github@gmail.com>
2014-01-09 00:52:22 +0100

    couple of templating examples #3

---
+++ jsf-showcases.web/src/main/java/hu/palkonyves/jsfshowcase/DynamicTemplateExample.java
@@ -0,0 +1,42 @@
+package hu.palkonyves.jsfshowcase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class DynamicTemplateExample {
+
+    private static List<String> templates;
+    private String selectedTemplate = templates.get(0);
+
+    static {
+        List<String> tmpls = new ArrayList<String>();
+        tmpls.add("/resources/templates/dynamicTemplate1.xhtml");
+        tmpls.add("/resources/templates/dynamicTemplate2.xhtml");
+
+        DynamicTemplateExample.templates = Collections
+                .unmodifiableList(tmpls);
+    }
+
+    protected DynamicTemplateExample() {
+
+    }
+
+    public List<String> getTemplates() {
+        return templates;
+    }
+
+    public String getSelectedTemplate() {
+        return selectedTemplate;
+    }
+
+    public void setSelectedTemplate(String template) {
+        selectedTemplate = template;
+    }
+
+}
