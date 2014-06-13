e298d05a36a9b1d96b5301526a54c5b5e7f2896d
Pal Konyves <paul.konyves@gmail.com>
2014-06-13 23:21:24 +0100

    Tomcat 7 container with redeployment capability

--- jsf-showcases.web/src/test/java/hu/palkonyves/jsfshowcase/TopicsTest.java
+++ jsf-showcases.web/src/test/java/hu/palkonyves/jsfshowcase/TopicsTest.java
@@ -5,34 +5,35 @@ import java.util.regex.Pattern;
 
 import junit.framework.Assert;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+@Ignore
 public class TopicsTest {
 
-    private static final Pattern H1_PATTERN = Pattern
-            .compile("\\<h1\\>(.*)\\</h1\\>");
+	private static final Pattern H1_PATTERN = Pattern
+			.compile("\\<h1\\>(.*)\\</h1\\>");
 
-    private static final String TOPIC = "toptop";
+	private static final String TOPIC = "toptop";
 
-    private final String source = "<html>\n" + "<body>\n"
-            + "<div> something </div>\n" + "<h1>" + TOPIC + "</h1>\n"
-            + "</body>\n" + "</html>";
+	private final String source = "<html>\n" + "<body>\n"
+			+ "<div> something </div>\n" + "<h1>" + TOPIC + "</h1>\n"
+			+ "</body>\n" + "</html>";
 
+	@Test
+	public void getTopicsTest() {
 
-    @Test
-    public void getTopicsTest() {
+		String[] lines = source.split("\\r?\\n");
+		String matching = null;
+		for (String line : lines) {
+			Matcher h1Matcher = H1_PATTERN.matcher(line);
+			if (h1Matcher.matches()) {
+				matching = h1Matcher.group(1);
+			}
+		}
 
-        String[] lines = source.split("\\r?\\n");
-        String matching = null;
-        for (String line : lines) {
-            Matcher h1Matcher = H1_PATTERN.matcher(line);
-            if (h1Matcher.matches()) {
-                matching = h1Matcher.group(1);
-            }
-        }
-
-        System.out.println(matching);
-        Assert.assertEquals(TOPIC, matching);
-    }
+		System.out.println(matching);
+		Assert.assertEquals(TOPIC, matching);
+	}
 
 }
