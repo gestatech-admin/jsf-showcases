23cf5b41eccb17d0457553d8e538c38238fe560a
pkonyves <paul.konyves@gmail.com>
2015-05-23 14:18:40 +0200

    Added basic JAX-RS service example

---
+++ jsf-showcases.web/src/main/java/hu/palkonyves/ws/rest/BananaService.java
@@ -0,0 +1,27 @@
+package hu.palkonyves.ws.rest;
+import hu.palkonyves.jsfshowcase.business.Banana;
+import hu.palkonyves.jsfshowcase.business.BananaStore;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@ApplicationScoped
+@Produces("application/json")
+@Path("bananaService")
+public class BananaService {
+    
+    @Inject
+    BananaStore bananaStore;
+
+    @GET
+    @Path("getAllBananas")
+    public List<Banana> getAllBananas() {
+        List<Banana> allBanana = bananaStore.getAllBanana();
+        return allBanana;
+    }
+}
