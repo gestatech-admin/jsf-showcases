4683c622984a3f0faae77566258a964922bee498
pkonyves <paul.konyves@gmail.com>
2015-06-21 12:31:47 +0200

    \#47 Modified swagger codegen for jaxrs
    -

--- jsf-showcases.web/swagger-generate-client.sh
+++ jsf-showcases.web/swagger-generate-client.sh
@@ -6,5 +6,5 @@ outputDir=$2
 # https://github.com/swagger-api/swagger-codegen
 java -cp `cat build-classpath.txt` io.swagger.codegen.SwaggerCodegen generate \
   -i $swaggerJson \
-  -l java \
+  -l jaxrs \
   -o $outputDir
