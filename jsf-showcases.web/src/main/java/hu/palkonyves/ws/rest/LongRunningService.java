9eee89e2e6dd84ed2675c7a18fb3393b97fe2e01
Pal <paul.konyves@gmail.com>
2016-08-19 04:33:14 +0700

    Added logger and reformatted LongRunningService
    in jsf-showcases.web

--- jsf-showcases.web/src/main/java/hu/palkonyves/ws/rest/LongRunningService.java
+++ jsf-showcases.web/src/main/java/hu/palkonyves/ws/rest/LongRunningService.java
@@ -2,6 +2,8 @@ package hu.palkonyves.ws.rest;
 
 import hu.palkonyves.ws.rest.dto.ElapsedTime;
 
+import java.util.logging.Logger;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -19,32 +21,33 @@ import com.wordnik.swagger.annotations.ApiResponse;
 @Produces("application/json")
 public class LongRunningService {
 
-    @GET
-    @Path("elapsedTime")
-    @ApiOperation(value = "Find purchase order by ID",
-    notes = "For valid response try integer IDs with value <= 5 or > 10. Other values will generated exceptions")
-  @ApiResponses(value = { @ApiResponse(code = 400, message = "Invalid ID supplied"),
-      @ApiResponse(code = 404, message = "Order not found") })
-    public ElapsedTime getElapsedTime(@QueryParam("maxMillisec") Long maxMillisec) {
-        ElapsedTime result = new ElapsedTime();
-        
-        if (maxMillisec == null) {
-            result.getErrors().add("maxMillisec must be provided");
-            return result;
-        }
+	private static Logger LOG = Logger.getLogger(LongRunningService.class.getName());
 
-        long startTime = System.nanoTime();
-        try {
-            Thread.sleep(maxMillisec);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        long endTime = System.nanoTime() - startTime;
-        
-        result.setElapsedTime(endTime);
-        result.setTimeUnit(ElapsedTime.TIME_UNIT_NS);
-        
-        return result;
-    }
+	@GET
+	@Path("elapsedTime")
+	@ApiOperation(value = "Find purchase order by ID", notes = "For valid response try integer IDs with value <= 5 or > 10. Other values will generated exceptions")
+	@ApiResponses(value = { @ApiResponse(code = 400, message = "Invalid ID supplied"),
+			@ApiResponse(code = 404, message = "Order not found") })
+	public ElapsedTime getElapsedTime(@QueryParam("maxMillisec") Long maxMillisec) {
+		ElapsedTime result = new ElapsedTime();
+
+		if (maxMillisec == null) {
+			result.getErrors().add("maxMillisec must be provided");
+			return result;
+		}
+
+		long startTime = System.nanoTime();
+		try {
+			Thread.sleep(maxMillisec);
+		} catch (InterruptedException e) {
+
+		}
+		long endTime = System.nanoTime() - startTime;
+
+		result.setElapsedTime(endTime / 1000);
+		result.setTimeUnit(ElapsedTime.TIME_UNIT_MS);
+
+		LOG.info("elapsedTime service returns");
+		return result;
+	}
 }
